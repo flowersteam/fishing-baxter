041b0cbcbb919e9a04f7160864d3602063a9b43d
Pierre Rouanet <pierre.rouanet@gmail.com>
2014-08-22 14:09:26 +0200

    Simple debug.

---
+++ fishing.py
@@ -0,0 +1,42 @@
+import os
+import time
+import rospy
+import argparse
+
+from numpy import array, save
+
+from .robot import FishingBaxter
+
+REPEAT = 10
+DURATION = 60 * 60
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--log', type=str, required=True)
+    args = parser.parse_args()
+
+    log = os.path.join('logs', args.log)
+    if os.path.exists(log):
+        raise IOError("Log file already esists!")
+
+    rospy.init_node('baxter_garde_la_peche')
+
+    baxter = FishingBaxter()
+    baxter.goes_to_init_position()
+
+    data = []
+
+    for _ in range(REPEAT):
+        start = time.time()
+
+        while time.time() - start < DURATION:
+            baxter.goes_to_init_position()
+
+            t = baxter.play_traj('traj.npy', record=True)
+            data.append(t.data)
+
+        time.sleep(DURATION)
+
+    data = array(data)
+    save(log, data)
---
+++ robot.py
@@ -0,0 +1,36 @@
+import sys
+
+sys.path.append('/home/baxtercontrol/dev/baxter_flowers/src/')
+
+from numpy import load
+
+from baxter_flowers import (Baxter,
+                            Trajectory, TrajectoryRecorder)
+
+
+class FishingBaxter(Baxter):
+    def __init__(self):
+        self.baxter = Baxter()
+
+    def goes_to_init_position(self):
+        self.play_traj('init_traj.npy')
+
+    def goes_to_end_position(self):
+        self.play_traj('end_traj.npy')
+
+    def play_traj(self, traj_file, record=False):
+        traj = Trajectory(self.baxter.left_arm,
+                          load('data/{}'.traj_file))
+
+        if record:
+            tr = TrajectoryRecorder(self.baxter.left_arm, 50)
+            tr.start()
+
+        traj.play()
+
+        traj.wait()
+        traj.stop()
+
+        if record:
+            tr.stop()
+            return tr.recorded_traj
